29919a335f17bc04bf964356d8073a117720a0c3
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-14 11:47:57 -0300

    Aula 142: Determining Error Type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/databse-connection-error";
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if (err instanceof RequestValidationError) {
+    console.log("handling this error as a request validator");
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log("handling this error as a db connection error");
+  }
 
   res.status(400).send({
-    message: err.message
+    message: err.message,
   });
 };
