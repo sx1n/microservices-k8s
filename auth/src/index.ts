ee827a082b2fa08dbc7d3ea4dd40f7fcb52c9d80
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-14 16:10:41 -0300

    Aula 149: How to Define New Custom Errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from "express";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -14,8 +15,13 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+  console.log("Listening on port 3000!!!!!!!!");
 });
