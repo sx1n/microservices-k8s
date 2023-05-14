02c0fdd901362f7945bbf19ef15dfed349be74f0
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-14 12:11:34 -0300

    Aula 144: Converting Errors to Responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/databse-connection-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -9,13 +9,19 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log("handling this error as a request validator");
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
+
   if (err instanceof DatabaseConnectionError) {
-    console.log("handling this error as a db connection error");
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: "Something went wrong" }],
   });
 };
