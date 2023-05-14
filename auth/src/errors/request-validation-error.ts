18fc0a818e13700125405c350cf7b8d9bef638b1
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-14 15:24:29 -0300

    Aula 145: Moving Logic Into Errors

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,19 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super();
 
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
   }
 }
